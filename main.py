2d99cd62d2a3ccb1cbb72907615c35026dcf4de2
Vladd <vl44el@yandex.ru>
2019-11-27 19:27:49 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 import sys
 import random
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QBrush
-from PyQt5.QtCore import Qt
-from PyQt5 import uic
+
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QLCDNumber, QMainWindow
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import QSize, Qt, QLine, QPoint
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(262, 325)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(90, 250, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 262, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Yellow"))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draw_line)
-        self.painter = QPainter(self)
-        self.sss = []
+        self.line = QLine()
 
     def draw_line(self):
-        self.sss = []
-        tt = random.randint(1, 5)
-        for i in range(tt):
-            a = random.randint(0, 200)
-            b = random.randint(0, 200)
-            b1 = random.randint(0, 200)
-            self.sss.append([a, b, b1])
+        button = self.sender()
+        self.line = QLine(QPoint(), button.pos())
         self.update()
 
     def paintEvent(self, event):
-        self.painter.begin(self)
-        if self.sss:
-            self.painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
-            for i in range(len(self.sss)):
-                self.painter.drawEllipse(self.sss[i][1], self.sss[i][2], self.sss[i][0], self.sss[i][0])
-        self.painter.end()
+        QMainWindow.paintEvent(self, event)
+        if not self.line.isNull():
+            painter = QPainter(self)
+            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            tt = random.randint(1, 5)
+            for i in range(tt):
+                a = random.randint(0, 200)
+                b = random.randint(0, 200)
+                b1 = random.randint(0, 200)
+                painter.drawEllipse(b, b1, a, a)
+
 
 
 if __name__ == '__main__':
