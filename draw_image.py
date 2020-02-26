5fc6000727563e7f2595ff333f1c4f042ab3f46b
Vladd <vl44el@yandex.ru>
2020-02-26 19:32:45 +0500

    Initial commit

--- draw_image.py
+++ draw_image.py
@@ -8,12 +8,13 @@ def draw_image(content):
     with open(map_file, 'wb') as file:
         file.write(get_map(content))
     pygame.init()
-    screen = pygame.display.set_mode((450, 450))
+    screen = pygame.display.set_mode((650, 450))
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
     running = True
     fps = 30
     clock = pygame.time.Clock()
+    t = tt = 0
     while running:
         clock.tick(fps)
         for event in pygame.event.get():
@@ -26,11 +27,20 @@ def draw_image(content):
                 elif event.button == pygame.BUTTON_LEFT:
                     if content["z"] > 1:
                         content["z"] -= 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    content["ll"] = str(float(content["ll"][:content["ll"].find(',')]) + content["z"] * 90 / 450) + content["ll"][content["ll"].find(',') + 1:]
+                elif event.key == pygame.K_RIGHT:
+                    content["ll"] = str(float(content["ll"][:content["ll"].find(',')]) + content["z"] * 90 / 450) + content["ll"][content["ll"].find(',') + 1:]
+                elif event.key == pygame.K_UP:
+                    content["ll"] = str(float(content["ll"][:content["ll"].find(',')]) + content["z"] * 90 / 450) + content["ll"][content["ll"].find(',') + 1:]
+                elif event.key == pygame.K_DOWN:
+                    content["ll"] = str(float(content["ll"][:content["ll"].find(',')]) + content["z"] * 90 / 450) + content["ll"][content["ll"].find(',') + 1:]
         with open(map_file, 'wb') as file:
             file.write(get_map(content))
         pygame.init()
-        screen = pygame.display.set_mode((450, 450))
-        screen.blit(pygame.image.load(map_file), (0, 0))
+        screen = pygame.display.set_mode((650, 450))
+        screen.blit(pygame.image.load(map_file), (t, tt))
         pygame.display.flip()
     pygame.quit()
     os.remove(map_file)
--- ppppppp.py
+++ ppppppp.py
@@ -2,7 +2,7 @@ from draw_image import draw_image
 
 
 address_ll = "37.588392,55.734036"
-size = '450,450'
+size = '650,450'
 
 map_params = {
     "ll": address_ll,
